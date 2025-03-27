97fb880015e6730047da2c82b9fba443ad1de26e
Nikhil Sehgal <nikhilsehgal419@gmail.com>
2025-03-27 13:14:15 +0530

    regression.py

--- mllake/regression.py
+++ mllake/regression.py
@@ -17,5 +17,5 @@ class Regressor:
 
     def evaluate(self, X_test, y_test):
         from sklearn.metrics import mean_squared_error
-        return mean_squared_error(y_test, self.model.predict(X_test), squared=False)
+        return mean_squared_error(y_test, self.model.predict(X_test)) ** 0.5  # Manually compute RMSE
 
